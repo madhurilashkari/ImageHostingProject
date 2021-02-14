a5cc794c1b222b5972885c585575c3c2039ad6d1
Madhuri <madhukkl2000@gmail.com>
2021-02-14 14:57:07 +0530

    Comment feature implementation

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
 public class ImageHosterApplication {
     public static void main(String[] args) {
